47a3ab709c2f5ae0425b6efc6c25193c9a6a4a13
EmanuelBaciu <62619090+EmanuelBaciu@users.noreply.github.com>
2020-03-25 18:41:37 +0200

    Add files via upload

---
+++ PersoaneContact.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    /// <summary>
+    /// Persoanele de contact din agenda
+    /// </summary>
+   public class PersoaneContact
+    {
+        public string nume;
+        public string numarTelefon;
+        public string adresaEmail;
+
+        public DateTime DataNasterii;
+        public PersoaneContact()
+        {
+            nume = string.Empty;
+            numarTelefon = string.Empty;
+            adresaEmail = string.Empty;
+            
+
+        }
+        /// <summary>
+        /// Constructor cu parametri
+        /// </summary>
+        /// <param name="numePersoana"></param>
+        /// <param name="nrTel"></param>
+        /// <param name="e_mail"></param>
+        public PersoaneContact(string numePersoana, string nrTel, string e_mail)
+       {
+            nume = numePersoana;
+          numarTelefon = nrTel;
+            adresaEmail = e_mail;
+
+        }
+        /// <summary>
+        /// Constructor care primeste un sir de caractere
+        /// </summary>
+        /// <param name="pers1"></param>
+        public PersoaneContact(string pers1)
+        {
+            string[] buff = pers1.Split();
+            nume = buff[0];
+            numarTelefon = buff[1];
+            adresaEmail = buff[2];
+
+        }
+
+        public string Info()
+        {
+            return "Persoana de contact are numele " + nume + " cu numarul de telefon: " + numarTelefon + " si adresa de e-mail: " + adresaEmail;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersoaneContact persoana = new PersoaneContact("Emanuel", "0743307248", "emy_baciu@yahoo.com");
+
+            
+            Console.WriteLine(persoana.Info());
+            Console.WriteLine(persoana.DataNasterii = new DateTime(1999, 01, 03));
+
+            Console.ReadKey();
+
+            PersoaneContact p1 = new PersoaneContact("Andreea", "0756818340", "andreea@yahoo.com");
+
+            Console.WriteLine(p1.Info());
+            Console.WriteLine(p1.DataNasterii = new DateTime(1999, 07, 31));
+
+
+            Console.ReadKey();
+        }
+    }
+}
